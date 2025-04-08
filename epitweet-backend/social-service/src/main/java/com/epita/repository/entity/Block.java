0a8fbf5ee50835617f0628196bc6b15593b7ce17
victor <victor.mandelaire@epita.fr>
2025-04-08 23:16:09 +0200

    fix<social-srvc>: fixed the fact that a user could like two times the same post, and added some comments

--- epitweet-backend/social-service/src/main/java/com/epita/repository/entity/Block.java
+++ epitweet-backend/social-service/src/main/java/com/epita/repository/entity/Block.java
@@ -5,11 +5,21 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * Represents the block of a user towards another user.
+ */
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 public class Block {
+    /**
+     * The ID of the user who is blocking.
+     */
     public String userId;
+
+    /**
+     * The ID of the user who is being blocked.
+     */
     public String userBlockedId;
 }
